d5f53f5ca1d0ba1035f8ffbb475fa8596eb3d311
Binura Ranasinghe <binuranavinda2000@gmail.com>
2023-11-04 20:55:11 +0530

    Updated the code

--- Scripts/script.js
+++ Scripts/script.js
@@ -58,7 +58,7 @@ function myFunction() {
 	});
 });*/
 
-$(document).ready(function() {
+/*$(document).ready(function() {
 
   // Get the navigation bar element
   var navBar = $('.topnav');
@@ -89,7 +89,7 @@ $(document).ready(function() {
       }
     });
   });
-});
+});*/
 
 // Get the navigation bar elements
 //const navBarElements = document.querySelector('.topnav').querySelectorAll('a');
@@ -98,6 +98,29 @@ $(document).ready(function() {
 /*navBarElements.forEach(element => {
   console.log(element);
 });*/
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+	sections.forEach(sec => {
+		let top = window.scrollY ;
+		let offset = sec.offsetTop;
+		let height = sec.offsetHeight;
+		let id = sec.getAttribute('id');
+		
+		if (top >= offset && top < offset + height) {
+			navLinks.forEach(links => {
+				links.classList.remove('active');
+				document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+			});
+		};
+	});
+};
+
+
+
+
 
 
 
